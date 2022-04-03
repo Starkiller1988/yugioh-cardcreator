2737a8553e80345a52f9b2a0178bb7b8fb4504f6
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-04-03 07:55:14 +0200

    complete the about page

--- frontend/src/components/AboutThisApp/AboutThisApp.tsx
+++ frontend/src/components/AboutThisApp/AboutThisApp.tsx
@@ -8,6 +8,21 @@ function AboutThisApp() {
         <Link to="/home"  className="home-link">
             Home
         </Link>
+
+        <div className="about-container">
+            <h1 className="about-title">
+              This tomb created in four weeks time,
+              <br />
+              <br />
+              the result is the hard work of mine.
+              <br />
+              <br />
+              Get ready for this dark renewal,
+              <br />
+              <br />
+              because it's time to DUEL!
+            </h1>
+        </div>
     </div>
   )
 }
