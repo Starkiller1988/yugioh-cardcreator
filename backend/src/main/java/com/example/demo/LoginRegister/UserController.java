9e448a26cab6664736dfc218ee3ffde83e407405
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-04-06 14:32:03 +0200

    fixed some things in application.properties

--- backend/src/main/java/com/example/demo/LoginRegister/UserController.java
+++ backend/src/main/java/com/example/demo/LoginRegister/UserController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 
 @RestController
